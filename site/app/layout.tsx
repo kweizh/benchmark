c8e0914e2de09dc6985cbb7de522c1f57eec678b
Wei Zhang <kweizh@tabbyml.com>
2026-03-21 00:16:21 +0800

    feat(site): sync upstream

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 
 import type { Metadata } from 'next';
 import { Inter } from "next/font/google";
-import zealtConfig from "../../zealt.json";
+import zealtConfig from "@/zealt/config.json";
 
 const inter = Inter({ subsets: ["latin"] });
 
